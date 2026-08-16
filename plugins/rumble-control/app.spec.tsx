6943a9a042b8e62952b2eff84feb4ebb319099ca
Welld1s <116039550+Welld1s@users.noreply.github.com>
2026-08-16 20:29:49 +0300

    Add initial test for Rumble Control UI

---
+++ plugins/rumble-control/app.spec.tsx
@@ -0,0 +1,10 @@
+import { describe, it, expect, mock, beforeEach } from "bun:test";
+import * as actualUi from "@loadout/ui";
+import { waitFor, fireEvent } from "../../test/render";
+
+// Моки и тесты можно добавить позже
+describe("Rumble Control UI", () => {
+  it("placeholder", () => {
+    expect(true).toBe(true);
+  });
+});
